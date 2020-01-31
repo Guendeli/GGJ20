1e1bd68a0b5af40c4df842196e89d4dd8055ec4c
Alwin_Joshy <flamencoresonance@gmail.com>
2020-01-31 17:41:36 +0400

    dound manager system done

---
+++ Assets/Alwin/Script/SoundData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class SoundData
+{
+    public string soundID;
+    public AudioClip audioClip;
+}
---
+++ Assets/Alwin/Script/SoundManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+    public SoundLibrary soundLibrary;
+    public List<AudioSource> activeSoundSourcePool =  new List<AudioSource>();
+    public List<AudioSource> inactiveSoundSourcePool = new List<AudioSource>();
+    void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        inactiveSoundSourcePool.Add(new GameObject().AddComponent(typeof(AudioSource)) as AudioSource);
+    }
+
+    public void PlaySound( SoundData soundModel)
+    {
+        if (inactiveSoundSourcePool.Count > 0) 
+        {
+            inactiveSoundSourcePool[0].clip = soundModel.audioClip;
+            inactiveSoundSourcePool[0].Play();
+            activeSoundSourcePool.Add(inactiveSoundSourcePool[0]);
+            inactiveSoundSourcePool.Remove(inactiveSoundSourcePool[0]);
+            return;
+        }
+        AudioSource newSoundSource = new GameObject().AddComponent(typeof(AudioSource)) as AudioSource;
+        newSoundSource.clip = soundModel.audioClip;
+        newSoundSource.Play();
+        inactiveSoundSourcePool.Add(newSoundSource);
+    }
+
+    void Update() 
+    {
+        for (int i = 0; i < activeSoundSourcePool.Count; i++)
+        {
+            if (!activeSoundSourcePool[i].isPlaying) 
+            {
+                inactiveSoundSourcePool.Add(activeSoundSourcePool[0]);
+                activeSoundSourcePool.Remove(activeSoundSourcePool[0]);
+            }
+        }
+    }
+        
+}
