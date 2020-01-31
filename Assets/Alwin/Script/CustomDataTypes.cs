0237ec411d2f352b9afe41c9bbc6869da8bf8981
Alwin_Joshy <flamencoresonance@gmail.com>
2020-01-31 09:50:08 +0400

    key pad puzzle at Alwin Scene

---
+++ Assets/Alwin/Script/CustomDataTypes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class SymbolButton
+{
+    public Sprite image;
+    public int id;
+}
+
+[System.Serializable]
+public class SymbolColumn
+{
+    public SymbolButton[] symbols;
+}
