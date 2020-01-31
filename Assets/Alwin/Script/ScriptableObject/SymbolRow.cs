a992b9ee04ce9286ddff771eef215aed801179b1
Guendeli <guendeliomar@gmail.com>
2020-01-31 10:40:01 +0400

    refactored alwin scripts and moved to SymbolKayPadModules

--- Assets/Alwin/Script/ScriptableObject/SymbolRow.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "symbolRow", menuName ="Custom/SymbolObject")]
-public class SymbolRow : ScriptableObject
-{
-    public SymbolColumn[] column;
-}
---
+++ Assets/Game/Scripts/Modules/SymbolKeyPadModule/Data/SymbolData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    [System.Serializable]
+    public class SymbolButton
+    {
+        public Sprite image;
+        public int id;
+    }
+
+    [System.Serializable]
+    public class SymbolColumn
+    {
+        public SymbolButton[] symbols;
+    }
+
+    [CreateAssetMenu(fileName = "SymbolDataSetting", menuName = "Data/SymbolDataSetting")]
+    public class SymbolData : ScriptableObject
+    {
+        public SymbolColumn[] column;
+    }
+
+
+}
