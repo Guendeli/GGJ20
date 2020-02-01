731cc228cd6495bec8de2bbb036e5bae187016fd
Guendeli <guendeliomar@gmail.com>
2020-02-01 13:54:48 +0400

    missing script

--- Assets/Game/Scripts/Core/StartRoundTutorialStep.cs
+++ Assets/Game/Scripts/Core/StartRoundTutorialStep.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartRoundTutorialStep : MonoBehaviour
+public class StartRoundTutorialStep : FTUEStep
 {
-    // Start is called before the first frame update
-    void Start()
+    public float FadeTime;
+    public float WaitTime;
+    private CanvasGroup canvasGroup;
+
+    public override IEnumerator OnExecute()
     {
-        
+        Hashtable hash = iTween.Hash("from", 0, "to", 1, "time", FadeTime, "onupdate", "Fade");
+        iTween.ValueTo(gameObject, hash);
+
+        while (!(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) || Input.GetKeyDown(KeyCode.Space)))
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        hash = iTween.Hash("from", 1, "to", 0, "time", FadeTime, "onupdate", "Fade");
+        iTween.ValueTo(gameObject, hash);
+        yield return new WaitForSeconds(WaitTime);
+        OnStepExit();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnStepExit()
     {
-        
+        base.OnStepExit();
+    }
+
+    public override void OnStepStart()
+    {
+        base.OnStepStart();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void Fade(float newValue)
+    {
+        canvasGroup.alpha = newValue;
     }
 }
