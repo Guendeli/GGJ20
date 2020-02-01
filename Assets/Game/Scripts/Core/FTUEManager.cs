4d004f9ec505fcf31454d8be9b1ae0ca35d81fda
Guendeli <guendeliomar@gmail.com>
2020-02-01 13:30:21 +0400

    Tutorial Manager

---
+++ Assets/Game/Scripts/Core/FTUEManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FTUEManager : MonoBehaviour
+{
+    public static FTUEManager Instance;
+    public List<FTUEStep> _steps;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        InitSteps();
+        StartCoroutine(Execute());
+    }
+
+    private void InitSteps()
+    {
+
+    }
+
+    public IEnumerator Execute()
+    {
+        foreach (FTUEStep step in _steps)
+        {
+            step.OnStepStart();
+            StartCoroutine(step.OnExecute());
+            while (!step.IsFinished)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+        }    
+    }
+}
---
+++ Assets/Game/Scripts/Core/FadableTutorialStep.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadableTutorialStep : FTUEStep
+{
+
+    public float FadeTime;
+    public float WaitTime;
+    private CanvasGroup canvasGroup;
+
+    public override IEnumerator OnExecute()
+    {
+        Hashtable hash = iTween.Hash("from", 0, "to", 1, "time", FadeTime, "onupdate", "Fade");
+        iTween.ValueTo(gameObject, hash);
+        yield return new WaitForSeconds(WaitTime);
+        hash = iTween.Hash("from", 1, "to", 0, "time", FadeTime, "onupdate", "Fade");
+        iTween.ValueTo(gameObject, hash);
+        yield return new WaitForSeconds(WaitTime);
+        OnStepExit();
+    }
+
+    public override void OnStepExit()
+    {
+        base.OnStepExit();
+    }
+
+    public override void OnStepStart()
+    {
+        base.OnStepStart();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    #region Helper MEthods
+    public void Fade(float newValue)
+    {
+        canvasGroup.alpha = newValue;
+    }
+
+
+    #endregion 
+}
